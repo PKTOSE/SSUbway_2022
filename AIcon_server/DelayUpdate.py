4a60a0bf1bd09152e26642dbad6649a584b4cb27
Jcshin <jcshin2003@naver.com>
2022-11-06 20:53:42 +0900

    서버에 지연 정보 업로드 모델

--- AIcon_server/DelayUpdate.py
+++ AIcon_server/DelayUpdate.py
@@ -1,25 +1,59 @@
 from models.delayModel import delayModel
 import json
-import time
-import datetime
 
 class DelayUpdate :
     def __init__(self) :
         self.delaymodel = delayModel()
-        self.initalize()
+        self.initialize()
 
-    def initialize() :
+    def initialize(self) :
         self.delayInfo = {
-            "예정":{}
-            "진행중"{}
-            "종료":{}
-            "오래됨":{}
+           
         }
 
-    def requstDelayInfo(self,data) :
+    # def requstDelayInfo(self,data) :
+    #     with open("SSUbway_2022/AIcon_server/delayData.json","rt" , encoding= "UTF8") as f :
+    #         delayData = json.load(f)
+
+    def parse(self,delayData) :
+        date = delayData["date"]
+        time = delayData["time"]
+        info = delayData["info"]
+        line = delayData["line"]
+        updatedData = {
+            "시간" : time, 
+            "정보" : info,
+            "호선" : line
+        }
+        self.delayInfo[date] = updatedData
+
+    def parseRequest(self , JSON_FILE) :
+        delaySatuses = JSON_FILE
+
+        for infos in delaySatuses :
+            self.parse(infos)
+        self.delaymodel.updateDelayInfo(self.delayInfo)
+
+    def run(self) :
+        """
+        run을 실행하면 자동으로 지연정보가 올려지도록
+        """
+        self.initialize()
+        self.delaymodel.remove()
+        
         with open("SSUbway_2022/AIcon_server/delayData.json","rt" , encoding= "UTF8") as f :
-            delayData = json.load(f)
+            JSON_FILE = json.load(f)
+        try :
+            self.parseRequest(JSON_FILE)
+            return True
+        except :
+            print("error")
+            False
 
 
 
 
+
+if __name__ == "__main__" :
+    testInstance = DelayUpdate()
+    testInstance.run()
